fcc73b24908705e57395f8034c615e4635604236
KamiliArsyad <arsyad.ik@gmail.com>
2025-03-24 12:46:37 +0800

    Improve scheduler randomness

--- storage/innobase/sched/sched0sched.cc
+++ storage/innobase/sched/sched0sched.cc
@@ -9,6 +9,8 @@
 
 typedef std::pair<int, trx_t *> TrxPriority;
 
+static std::mt19937 rng;
+static std::atomic<bool> rng_initialized;
 static std::mutex scheduler_mutex;
 static std::condition_variable scheduler_cv;
 
@@ -24,6 +26,27 @@ struct CompareTrxPriority {
 };
 
 static std::priority_queue<TrxPriority, std::vector<TrxPriority>, CompareTrxPriority> scheduler_queue;
+
+// The initialization will race, but it is fine as they are storing the same thing.
+static void init_rng() {
+  if (rng_initialized.load(std::memory_order::acq_rel)) return;
+
+  const char* seed_str = std::getenv("RANDOM_SEED");
+  int seed = 42;
+  if (seed_str != nullptr) seed = std::stoi(seed_str);
+
+  rng.seed(seed);
+  rng_initialized.store(true, std::memory_order::acq_rel);
+}
+
+static int get_random_priority() {
+  init_rng();
+  std::uniform_int_distribution dist(0, INT_MAX);
+
+  return dist(rng);
+}
+
+constexpr int PERIODIC_WAKE = 10;
 
 static void trx_scheduler_run() {
   while (scheduler_running.load(std::memory_order_acquire)) {
@@ -53,7 +76,7 @@ void trx_scheduler_request(trx_t *trx, event_type_t event_type) {
 
   // Put into queue if not yet
   if (!scheduler_blocked.contains(trx)) {
-    int rnd_priority = rand();
+    int rnd_priority = get_random_priority();
     scheduler_queue.push({rnd_priority, trx});
     scheduler_blocked[trx] = true;
     scheduler_cv.notify_all();
